9b803890a78595de953536b802532f6a69837da0
Shayne Fletcher <shayne@shaynefletcher.org>
2020-04-24 14:55:19 -0400

    reorder decls

--- scala-exp/akka-quickstart-scala/src/main/scala/com/example/Device.scala
+++ scala-exp/akka-quickstart-scala/src/main/scala/com/example/Device.scala
@@ -80,51 +80,6 @@ object DeviceManager {
   private final case class DeviceGroupTerminated(groupId: String) extends DeviceManager.Command
 }
 
-class DeviceManager(context: ActorContext[DeviceManager.Command])
-    extends AbstractBehavior[DeviceManager.Command](context) {
-  import DeviceManager._
-
-  var groupIdToActor = Map.empty[String, ActorRef[DeviceGroup.Command]]
-
-  context.log.info("DeviceManager started")
-
-  override def onMessage(msg: Command): Behavior[Command] =
-    msg match {
-      case trackMsg @ RequestTrackDevice(groupId, _, replyTo) =>
-        groupIdToActor.get(groupId) match {
-          case Some(ref) =>
-            ref ! trackMsg
-          case None =>
-            context.log.info("Creating device group actor for {}", groupId)
-            val groupActor = context.spawn(DeviceGroup(groupId), "group-" + groupId)
-            context.watchWith(groupActor, DeviceGroupTerminated(groupId))
-            groupActor ! trackMsg
-            groupIdToActor += groupId -> groupActor
-        }
-        this
-
-      case req @ RequestDeviceList(requestId, groupId, replyTo) =>
-        groupIdToActor.get(groupId) match {
-          case Some(ref) =>
-            ref ! req
-          case None =>
-            replyTo ! ReplyDeviceList(requestId, Set.empty)
-        }
-        this
-
-      case DeviceGroupTerminated(groupId) =>
-        context.log.info("Device group actor for {} has been terminated", groupId)
-        groupIdToActor -= groupId
-        this
-    }
-
-  override def onSignal: PartialFunction[Signal, Behavior[Command]] = {
-    case PostStop =>
-      context.log.info("DeviceManager stopped")
-      this
-  }
-}
-
 object DeviceGroup {
   def apply(groupId: String): Behavior[Command] =
     Behaviors.setup(context => new DeviceGroup(context, groupId))
@@ -167,6 +122,51 @@ class DeviceGroup(context: ActorContext[DeviceGroup.Command], groupId: String)
   override def onSignal: PartialFunction[Signal, Behavior[Command]] = {
     case PostStop =>
       context.log.info("DeviceGroup {} stopped", groupId)
+      this
+  }
+}
+
+class DeviceManager(context: ActorContext[DeviceManager.Command])
+    extends AbstractBehavior[DeviceManager.Command](context) {
+  import DeviceManager._
+
+  var groupIdToActor = Map.empty[String, ActorRef[DeviceGroup.Command]]
+
+  context.log.info("DeviceManager started")
+
+  override def onMessage(msg: Command): Behavior[Command] =
+    msg match {
+      case trackMsg @ RequestTrackDevice(groupId, _, replyTo) =>
+        groupIdToActor.get(groupId) match {
+          case Some(ref) =>
+            ref ! trackMsg
+          case None =>
+            context.log.info("Creating device group actor for {}", groupId)
+            val groupActor = context.spawn(DeviceGroup(groupId), "group-" + groupId)
+            context.watchWith(groupActor, DeviceGroupTerminated(groupId))
+            groupActor ! trackMsg
+            groupIdToActor += groupId -> groupActor
+        }
+        this
+
+      case req @ RequestDeviceList(requestId, groupId, replyTo) =>
+        groupIdToActor.get(groupId) match {
+          case Some(ref) =>
+            ref ! req
+          case None =>
+            replyTo ! ReplyDeviceList(requestId, Set.empty)
+        }
+        this
+
+      case DeviceGroupTerminated(groupId) =>
+        context.log.info("Device group actor for {} has been terminated", groupId)
+        groupIdToActor -= groupId
+        this
+    }
+
+  override def onSignal: PartialFunction[Signal, Behavior[Command]] = {
+    case PostStop =>
+      context.log.info("DeviceManager stopped")
       this
   }
 }
