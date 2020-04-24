a41de1fd8aec0d0a403fa4398adadf9f9bcab5b0
Shayne Fletcher <shayne@shaynefletcher.org>
2020-04-24 17:40:06 -0400

    finish the tutorial

--- scala-exp/akka-quickstart-scala/src/main/scala/com/example/Device.scala
+++ scala-exp/akka-quickstart-scala/src/main/scala/com/example/Device.scala
@@ -8,6 +8,8 @@ import akka.actor.typed.scaladsl.AbstractBehavior
 import akka.actor.typed.scaladsl.ActorContext
 import akka.actor.typed.scaladsl.Behaviors
 import akka.actor.typed.scaladsl.LoggerOps
+import akka.actor.typed.scaladsl.TimerScheduler
+import scala.concurrent.duration._
 
 object Device {
   def apply(groupId: String, deviceId: String): Behavior[Command] =
@@ -16,7 +18,7 @@ object Device {
   sealed trait Command
 
   final case class ReadTemperature(requestId: Long, replyTo: ActorRef[RespondTemperature]) extends Command
-  final case class RespondTemperature(requestId: Long, value: Option[Double])
+  final case class RespondTemperature(requestId: Long, deviceId: String, value: Option[Double])
 
   final case class RecordTemperature(requestId: Long, value: Double, replyTo: ActorRef[TemperatureRecorded])
       extends Command
@@ -42,7 +44,7 @@ class Device(context: ActorContext[Device.Command], groupId: String, deviceId: S
         this
 
       case ReadTemperature(id, replyTo) =>
-        replyTo ! RespondTemperature(id, lastTemperatureReading)
+        replyTo ! RespondTemperature(id, deviceId, lastTemperatureReading)
         this
 
       case Passivate =>
@@ -78,6 +80,19 @@ object DeviceManager {
   final case class ReplyDeviceList(requestId: Long, ids: Set[String])
 
   private final case class DeviceGroupTerminated(groupId: String) extends DeviceManager.Command
+
+  final case class RequestAllTemperatures(requestId: Long, groupId: String, replyTo: ActorRef[RespondAllTemperatures])
+      extends DeviceGroupQuery.Command
+      with DeviceGroup.Command
+      with DeviceManager.Command
+
+  final case class RespondAllTemperatures(requestId: Long, temperatures: Map[String, TemperatureReading])
+
+  sealed trait TemperatureReading
+  final case class Temperature(value: Double) extends TemperatureReading
+  case object TemperatureNotAvailable extends TemperatureReading
+  case object DeviceNotAvailable extends TemperatureReading
+  case object DeviceTimedOut extends TemperatureReading
 }
 
 object DeviceGroup {
@@ -182,5 +197,102 @@ class DeviceManager(context: ActorContext[DeviceManager.Command])
     case PostStop =>
       context.log.info("DeviceManager stopped")
       this
+  }
+}
+
+object DeviceGroupQuery {
+
+  def apply(
+      deviceIdToActor: Map[String, ActorRef[Device.Command]],
+      requestId: Long,
+      requester: ActorRef[DeviceManager.RespondAllTemperatures],
+      timeout: FiniteDuration): Behavior[Command] = {
+    Behaviors.setup { context =>
+      Behaviors.withTimers { timers =>
+        new DeviceGroupQuery(deviceIdToActor, requestId, requester, timeout, context, timers)
+      }
+    }
+  }
+
+  trait Command
+
+  private case object CollectionTimeout extends Command
+
+  final case class WrappedRespondTemperature(response: Device.RespondTemperature) extends Command
+
+  private final case class DeviceTerminated(deviceId: String) extends Command
+}
+
+class DeviceGroupQuery(
+    deviceIdToActor: Map[String, ActorRef[Device.Command]],
+    requestId: Long,
+    requester: ActorRef[DeviceManager.RespondAllTemperatures],
+    timeout: FiniteDuration,
+    context: ActorContext[DeviceGroupQuery.Command],
+    timers: TimerScheduler[DeviceGroupQuery.Command])
+    extends AbstractBehavior[DeviceGroupQuery.Command](context) {
+
+  import DeviceGroupQuery._
+  import DeviceManager.DeviceNotAvailable
+  import DeviceManager.DeviceTimedOut
+  import DeviceManager.RespondAllTemperatures
+  import DeviceManager.Temperature
+  import DeviceManager.TemperatureNotAvailable
+  import DeviceManager.TemperatureReading
+
+  timers.startSingleTimer(CollectionTimeout, CollectionTimeout, timeout)
+
+  private val respondTemperatureAdapter = context.messageAdapter(WrappedRespondTemperature.apply)
+
+  deviceIdToActor.foreach {
+    case (deviceId, device) =>
+      context.watchWith(device, DeviceTerminated(deviceId))
+      device ! Device.ReadTemperature(0, respondTemperatureAdapter)
+  }
+
+  private var repliesSoFar = Map.empty[String, TemperatureReading]
+  private var stillWaiting = deviceIdToActor.keySet
+
+  override def onMessage(msg: Command): Behavior[Command] =
+    msg match {
+      case WrappedRespondTemperature(response) => onRespondTemperature(response)
+      case DeviceTerminated(deviceId)          => onDeviceTerminated(deviceId)
+      case CollectionTimeout                   => onCollectionTimout()
+    }
+
+  private def onRespondTemperature(response: Device.RespondTemperature): Behavior[Command] = {
+    val reading = response.value match {
+      case Some(value) => Temperature(value)
+      case None        => TemperatureNotAvailable
+    }
+
+    val deviceId = response.deviceId
+    repliesSoFar += (deviceId -> reading)
+    stillWaiting -= deviceId
+
+    respondWhenAllCollected()
+  }
+
+  private def onDeviceTerminated(deviceId: String): Behavior[Command] = {
+    if (stillWaiting(deviceId)) {
+      repliesSoFar += (deviceId -> DeviceNotAvailable)
+      stillWaiting -= deviceId
+    }
+    respondWhenAllCollected()
+  }
+
+  private def onCollectionTimout(): Behavior[Command] = {
+    repliesSoFar ++= stillWaiting.map(deviceId => deviceId -> DeviceTimedOut)
+    stillWaiting = Set.empty
+    respondWhenAllCollected()
+  }
+
+  private def respondWhenAllCollected(): Behavior[Command] = {
+    if (stillWaiting.isEmpty) {
+      requester ! RespondAllTemperatures(requestId, repliesSoFar)
+      Behaviors.stopped
+    } else {
+      this
+    }
   }
 }
--- scala-exp/akka-quickstart-scala/src/test/scala/com/example/DeviceSpec.scala
+++ scala-exp/akka-quickstart-scala/src/test/scala/com/example/DeviceSpec.scala
@@ -135,3 +135,34 @@ class DeviceGroupSpec extends ScalaTestWithActorTestKit with WordSpecLike {
     }
   }
 }
+
+class DeviceGroupQuerySpec extends ScalaTestWithActorTestKit with WordSpecLike {
+  import Device.Command
+  import DeviceManager.{Temperature, RespondAllTemperatures}
+  import DeviceGroupQuery.WrappedRespondTemperature
+
+  "Device group query" must {
+    "return temperature value for working devices" in {
+      val requester = createTestProbe[RespondAllTemperatures]()
+
+      val device1 = createTestProbe[Command]()
+      val device2 = createTestProbe[Command]()
+
+      val deviceIdToActor = Map("device1" -> device1.ref, "device2" -> device2.ref)
+
+      val queryActor =
+        spawn(DeviceGroupQuery(deviceIdToActor, requestId = 1, requester = requester.ref, timeout = 3.seconds))
+
+      device1.expectMessageType[Device.ReadTemperature]
+      device2.expectMessageType[Device.ReadTemperature]
+
+      queryActor ! WrappedRespondTemperature(Device.RespondTemperature(requestId = 0, "device1", Some(1.0)))
+      queryActor ! WrappedRespondTemperature(Device.RespondTemperature(requestId = 0, "device2", Some(2.0)))
+
+      requester.expectMessage(
+        RespondAllTemperatures(
+          requestId = 1,
+          temperatures = Map("device1" -> Temperature(1.0), "device2" -> Temperature(2.0))))
+    }
+  }
+}
